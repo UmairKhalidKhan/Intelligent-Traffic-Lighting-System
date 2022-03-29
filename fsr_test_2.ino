aa85a086cedc321d5480ff050275567ea09bfaaa
UmairKhalidKhan <100151890+UmairKhalidKhan@users.noreply.github.com>
2022-03-29 20:04:45 +0500

    Add files via upload
    Test code to turn and LED light on when a fsr sensor is pressed.

---
+++ fsr_test_2.ino
@@ -0,0 +1,19 @@
+#define fsrpin 4
+#define ledpin 13
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledpin, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int input = analogRead(fsrpin);
+Serial.println(input);
+if(input > 20){
+  digitalWrite(ledpin, HIGH);
+}
+ delay(2000);
+digitalWrite(ledpin, LOW);
+delay(2000);
+}
